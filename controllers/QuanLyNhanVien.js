78932b0581b5f35c6da680f37bf67ba08eaf0b68
HauPham <xuanhau031297@gmail.com>
2020-06-11 19:14:47 +0700

    hauasdas

--- controllers/QuanLyNhanVien.js
+++ controllers/QuanLyNhanVien.js
@@ -1,5 +1,9 @@
 var DanhSachSinhVien = function() {
     console.log("Hello");
     alert(123);
+
 }
 DanhSachSinhVien();
+var HauLam = function() {
+    document.querySelector('#123').nodeValue;
+}
